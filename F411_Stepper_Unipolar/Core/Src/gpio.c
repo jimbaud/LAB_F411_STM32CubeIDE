c8580132287dabcaf8af4a1da47863312717fa9b
Joel Imbaud <64850076+jimbaud@users.noreply.github.com>
2025-09-16 09:56:18 +0200

    Add files via upload

--- F411_Stepper_Unipolar/Core/Src/gpio.c
+++ F411_Stepper_Unipolar/Core/Src/gpio.c
@@ -59,27 +59,27 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(Stepper_In3_A3_GPIO_Port, Stepper_In3_A3_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : PtPin */
+  /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PCPin PCPin */
+  /*Configure GPIO pins : Stepper_In1_A5_Pin Stepper_In2_A4_Pin */
   GPIO_InitStruct.Pin = Stepper_In1_A5_Pin|Stepper_In2_A4_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PAPin PAPin */
+  /*Configure GPIO pins : Stepper_In4_A2_Pin LD2_Pin */
   GPIO_InitStruct.Pin = Stepper_In4_A2_Pin|LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PtPin */
+  /*Configure GPIO pin : Stepper_In3_A3_Pin */
   GPIO_InitStruct.Pin = Stepper_In3_A3_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
